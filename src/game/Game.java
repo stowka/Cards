5635f6e6e980c0aafc2c8612a9bac31abc3c0ab0
Antoine De Gieter <antoine.degieter@net-production.ch>
2014-11-26 14:23:57 +0100

    First commit

---
+++ src/game/Game.java
@@ -0,0 +1,9 @@
+package game;
+
+public class Game {
+	public static void main(String[] argv) {
+		System.out.println("Game started...");
+		System.out.println("Game running...");
+		System.out.println("Game ended...");
+	}
+}
