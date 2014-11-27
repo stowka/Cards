f9541a35d6d562607b39fa8c0d6d31f34a802e02
MrPwet <beligat.fx@gmail.com>
2014-11-27 16:18:05 +0100

    basic Client

--- src/client/Client.java
+++ src/client/Client.java
@@ -1,9 +1,50 @@
 package client;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 public class Client {
 	
-	public static void main(String args[])
-	{
-		System.out.println("lol");
+	private String hostname;
+	private int port;
+	private Socket socketClient;
+	
+	public Client(String hostname, int port) {
+		this.hostname = hostname;
+		this.port = port;
+	}
+	
+	public void connect() throws UnknownHostException, IOException {
+		System.out.println("Attempting to connect to " + hostname + ":" + port);
+		socketClient = new Socket(hostname, port);
+		System.out.println("Connection established !");
+	}
+	
+	public void readResponse() throws IOException {
+		String userInput;
+		BufferedReader stdIn = new BufferedReader(new InputStreamReader(socketClient.getInputStream()));
+		
+		System.out.println("Response from server : ");
+		
+		while ((userInput = stdIn.readLine()) != null) {
+			System.out.println(userInput);
+		}
+	}
+	
+	public static void main(String args[]) {
+		Client client = new Client("127.0.0.1", 8888);
+		
+		try {
+			client.connect();
+			client.readResponse();
+		} catch (UnknownHostException e) {
+			System.err.println("Host unknown !");
+		} catch (IOException e) {
+			System.err.println("Cannot establish connection. Server may be down.");
+		}
+		
 	}
 }
