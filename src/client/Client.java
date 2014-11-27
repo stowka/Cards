a2bbd54937eab82653866dff9662b1be57ed8458
Antoine De Gieter <antoine.degieter@net-production.ch>
2014-11-27 16:31:39 +0100

    Server client connection

--- src/client/Client.java
+++ src/client/Client.java
@@ -18,33 +18,19 @@ public class Client {
 	}
 	
 	public void connect() throws UnknownHostException, IOException {
-		System.out.println("Attempting to connect to " + hostname + ":" + port);
+		System.out.println("[CLIENT] Attempting to connect to " + hostname + ":" + port);
 		socketClient = new Socket(hostname, port);
-		System.out.println("Connection established !");
+		System.out.println("[CLIENT] Connection established !");
 	}
 	
 	public void readResponse() throws IOException {
 		String userInput;
 		BufferedReader stdIn = new BufferedReader(new InputStreamReader(socketClient.getInputStream()));
 		
-		System.out.println("Response from server : ");
+		System.out.println("[CLIENT] Response from server : ");
 		
 		while ((userInput = stdIn.readLine()) != null) {
 			System.out.println(userInput);
 		}
-	}
-	
-	public static void main(String args[]) {
-		Client client = new Client("127.0.0.1", 8888);
-		
-		try {
-			client.connect();
-			client.readResponse();
-		} catch (UnknownHostException e) {
-			System.err.println("Host unknown !");
-		} catch (IOException e) {
-			System.err.println("Cannot establish connection. Server may be down.");
-		}
-		
 	}
 }
