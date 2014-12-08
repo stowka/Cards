fab7544b01c2eba310da7166f36ccb949e7bf1bd
Antoine De Gieter <antoine.degieter@net-production.ch>
2014-12-08 13:41:01 +0100

    I don't remember

--- src/test/Test.java
+++ src/test/Test.java
@@ -1,22 +1,9 @@
 package test;
 
-import java.io.IOException;
-import java.net.UnknownHostException;
-
 import server.Server;
-import client.Client;
 
 public class Test {
 	public static void main(String[] argv) {
 		new Thread(new Server()).start();
-		try {
-			Client client = new Client("127.0.0.1", 8888);
-			client.connect();
-			client.readResponse();
-		} catch (UnknownHostException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
 	}
 }
